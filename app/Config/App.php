eb28cbb211528f56d72f457c742dc6b503784c0c
Arvisco <94063972+Arvisco@users.noreply.github.com>
2022-03-12 23:32:53 +0800

    Major push

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/Webtiketpesawat(ci4)/';
+    public $baseURL = 'https://localhost/Webtiketpesawat/';
 
     /**
      * --------------------------------------------------------------------------
---
+++ app/Views/styler/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Vanilla Bootstrap v4.2.1 Theme</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href=" <?= base_url('/styler/Indexstyler/bootstrap.min.css')?> ">
+  
+        <link rel="stylesheet" href="<?= base_url('/styler/Indexstyler/fontAwesome.css')?>">
+        <link rel="stylesheet" href="<?= base_url('/styler/Indexstyler/hero-slider.css')?>">
+        <link rel="stylesheet" href="<?= base_url('/styler/Indexstyler/templatemo-main.css')?>">
+        <link rel="stylesheet" href="<?=base_url('/styler/Indexstyler/owl-carousel.css')?>">
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+        <script src="<?= base_url('/styler/Indexstyler/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js')?>"></script>
+    </head>
+<body>
